f88629090b494ed353ccb3724abca7546b201b32
Justin Campbell <justin@justincampbell.me>
2011-11-08 22:53:30 -0500

    Basic parser

--- lib/vector_victor/parser.rb
+++ lib/vector_victor/parser.rb
@@ -1,9 +1,31 @@
 module VectorVictor
 
   class Parser
+    attr_accessor :filename
 
-    def initialize
+    def initialize(options)
+      @filename = options[:filename]
+    end
 
+    def parse
+      @file = open @filename
+
+      while line = @file.read(32)
+        parse_line unpack_line(line)
+      end
+    end
+
+    def self.unpack_line(line)
+      line.unpack("N" * 8)
+    end
+
+    def self.parse_line(line)
+      {
+        :timestamp => line[2],
+        :traffic   => line[4],
+        :interest  => line[6],
+        :checksum  => line[7]
+      }
     end
 
   end
--- test/vector_victor/parser_test.rb
+++ test/vector_victor/parser_test.rb
@@ -6,4 +6,22 @@ class ParserTest < Test::Unit::TestCase
     assert VectorVictor::Parser.is_a? Class
   end
 
+  def test_unpack_line
+    line = VectorVictor::Parser.unpack_line open("test/data/1.log").read
+
+    assert line.is_a? Array
+    assert_equal 8, line.size
+  end
+
+  def test_parse_line
+    line = VectorVictor::Parser.unpack_line open("test/data/1.log").read
+    data = VectorVictor::Parser.parse_line line
+
+    assert data.is_a? Hash
+    assert_equal 1310443200, data[:timestamp]
+    assert_equal 1056924685, data[:traffic]
+    assert_equal      23590, data[:interest]
+    assert_equal 3867635626, data[:checksum]
+  end
+
 end
